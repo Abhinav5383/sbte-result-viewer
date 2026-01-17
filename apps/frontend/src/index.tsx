f39deb6cb29652ec4e663aee86487fee29934eba
Abhinav5383 <itsabhinav5383@gmail.com>
2026-01-17 17:56:44 +0530

    added tailwind

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./app.tsx";
+import "./index.css";
 
 const root = document.getElementById("root");
 if (root) render(() => <App />, root);
