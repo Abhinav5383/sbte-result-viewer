b6096fa29542812cae8bceb40d2540c169e43892
Abhinav5383 <itsabhinav5383@gmail.com>
2026-06-09 22:25:21 +0530

    Add @solidjs/router to work with searchParams

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,7 +1,16 @@
 /* @refresh reload */
+import { Route, Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 import App from "./app.tsx";
 import "./index.css";
 
 const root = document.getElementById("root");
-if (root) render(() => <App />, root);
+if (root)
+    render(
+        () => (
+            <Router>
+                <Route path="/" component={App} />
+            </Router>
+        ),
+        root,
+    );
