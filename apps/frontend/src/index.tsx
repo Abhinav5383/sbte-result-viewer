3a71974661a839d22faccc883d462c990fba815e
Abhinav5383 <itsabhinav5383@gmail.com>
2026-06-09 23:31:27 +0530

    remove useless log

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -4,8 +4,6 @@ import { render } from "solid-js/web";
 import App from "./app.tsx";
 import "./index.css";
 
-console.log(import.meta.env);
-
 const root = document.getElementById("root");
 if (root) {
     render(() => <Router root={App} />, root);
