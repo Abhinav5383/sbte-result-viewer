8e5f31701d4e79d85bc0ca1ed99776444f04f532
Abhinav5383 <itsabhinav5383@gmail.com>
2026-06-09 23:27:32 +0530

    fix mismatching base in gh pages deployment

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,16 +1,12 @@
 /* @refresh reload */
-import { Route, Router } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 import App from "./app.tsx";
 import "./index.css";
 
+console.log(import.meta.env);
+
 const root = document.getElementById("root");
-if (root)
-    render(
-        () => (
-            <Router>
-                <Route path="/" component={App} />
-            </Router>
-        ),
-        root,
-    );
+if (root) {
+    render(() => <Router root={App} />, root);
+}
