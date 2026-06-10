e720cbfd05f3060b797aabcb354f35a68b9a319f
Abhinav5383 <itsabhinav5383@gmail.com>
2026-06-10 10:37:57 +0530

    embed analytics script directly instead of linking

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,10 +1,19 @@
 /* @refresh reload */
 import { Router } from "@solidjs/router";
 import { render } from "solid-js/web";
+import analyticsScript from "~/lib/umalytics.js?raw";
 import App from "./app.tsx";
 import "./index.css";
 
 const root = document.getElementById("root");
 if (root) {
     render(() => <Router root={App} />, root);
+}
+
+if (!import.meta.env.DEV) {
+    const script = document.createElement("script");
+    script.setAttribute("data-website-id", "e9826197-f32b-4fa6-9df2-1691dada9ff4");
+    script.innerHTML = analyticsScript;
+
+    document.head.appendChild(script);
 }
