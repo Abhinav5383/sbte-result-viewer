8bbb41f970558c1ecacff344e83f3cc92e51d6a2
Abhinav <119372409+Abhinav5383@users.noreply.github.com>
2026-07-23 00:28:40 +0530

    Update student-picker.tsx

--- apps/frontend/src/pages/tools/group-generator/student-picker.tsx
+++ apps/frontend/src/pages/tools/group-generator/student-picker.tsx
@@ -39,7 +39,7 @@ export function StudentPickerDialog(props: StudentPickerDialogProps) {
             open={props.isOpen}
             onClose={props.onClose}
             dialogProps={{
-                class: "rounded-lg max-w-[100vw] sm:max-w-[calc(100vw-4rem)]",
+                class: "rounded-lg max-w-[calc(100vw_-_1rem)] sm:max-w-[calc(100vw_-_4rem)]",
                 ref: (el) => setDialogRef(el),
             }}
         >
