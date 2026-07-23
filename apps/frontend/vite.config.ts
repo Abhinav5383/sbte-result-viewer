1ce8078f8de6b699e8189fc2e3b54b89e1619dfa
Abhinav5383 <itsabhinav5383@gmail.com>
2026-07-23 11:51:35 +0530

    include small files in the index.html file

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,23 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-    plugins: [tailwindcss(), solid()],
+    plugins: [
+        tailwindcss(),
+        solid(),
+        viteSingleFile({
+            inlinePattern: ["**/index-*.js", "**/index-*.css"],
+            useRecommendedBuildConfig: true,
+            overrideConfig: {
+                build: {
+                    assetsDir: ""
+                }
+            }
+        }),
+    ],
     base: process.env.BASE_PATH,
 
     resolve: {
